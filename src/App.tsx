2261a9e194a8dc13c6ea26c7e2f94340ed32d742
Sharif Adal <sharifadal2008@gmail.com>
2026-05-10 11:43:04 +0600

    refactor(routing): integrate tanstack router into application entry

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,10 @@
+import { createRouter, RouterProvider } from "@tanstack/react-router";
+import { routeTree } from "./routeTree.gen";
+
+const router = createRouter({ routeTree });
+
 function App() {
-  return <h1 className="text-4xl text-blue-500 hover:text-2xl">Hello, Vite + React!</h1>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -2,15 +2,15 @@ import { createRootRoute, Link, Outlet } from "@tanstack/react-router";
 
 export const Route = createRootRoute({
   component: () => (
-    <div>
-      <header>
-        <h2>Hello, Vite + React!</h2>
-        <nav>
-          <Link to="/">Home</Link>
-          <Link to="/about">About</Link>
+    <div className="min-h-screen bg-gray-900 text-gray-50">
+      <header className="flex justify-between p-4 bg-slate-800">
+        <h2 className="text-4xl">Hello, Vite + React!</h2>
+        <nav className="text-2xl space-x-4 m-2">
+          <Link to="/" className="[&.active]:font-bold">Home</Link>
+          <Link to="/about" className="[&.active]:font-bold">About</Link>
         </nav>
       </header>
-      <main>
+      <main className="p-4">
         <Outlet />
       </main>
     </div>
